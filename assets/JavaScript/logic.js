9505a6aac64842493d53c9229b0de258587ba1f8
Aaron_G <gauntta@gmail.com>
2019-02-01 10:25:06 -0600

    updates

--- assets/JavaScript/logic.js
+++ assets/JavaScript/logic.js
@@ -2,6 +2,7 @@
 var topics = ["dogs", "cats", "monkeys", "squirrels", "snakes", "eagles"];
 var card = $("#body");
 var header = $("#heading");
+var APIkey = "CHJyzOiCX3h6rjniXg2nhoxLslu3hK8k"; 
 
 //array of animals in turned into buttons and propogates the top of the field
 function initialAnimals(){
@@ -11,6 +12,33 @@ function initialAnimals(){
     }
     $(document).on("click", ".animal-gif", function(){
         console.log(this.id);
+        var animal = this.id;
+        // $("#gif-area").text(this.id);
+        // console.log(animal);  
+
+        var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + animal + "&api_key=" + APIkey + "&limit=10";
+
+        $.ajax({
+            url: queryURL,
+            method: "GET"
+        })
+        .then(function(response) {
+            var results = response.data;
+    
+            for (var i = 0; i < results.length; i++) {
+                //var gifDiv = $("<div>");
+    
+                var rating = results[i].rating;
+    
+                var p = $("<p>").text("Rating: " + rating);
+    
+                var animalImage = $("<img>");
+                animalImage.attr("src", results[i].images.fixed_height.url);
+    
+                card.prepend(p);
+                card.prepend(animalImage);
+            }
+        });
     });
 }
  initialAnimals();
@@ -29,10 +57,11 @@ $("#submitNewAnimal").click(function(){
 })
     $(document).on("click", "#animal", function(){
         console.log(this.id);
-        animal = this.id;
-        $("#gif-area").append(this.id);
 
     });
 
-var queryURL = "https://api.giphy.com/v1/gifs/search?q=" 
+
+
+
+
 
